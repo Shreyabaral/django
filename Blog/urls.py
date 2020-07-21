8c2f67c00f8b7732e20f915f11cf295f7d658e3a
Shreya Baral <54298365+Shreyabaral@users.noreply.github.com>
2020-07-21 20:00:47 +0545

    Update urls.py

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'Blog'
 urlpatterns= [
-    path('',views.index.as_view(template_name="blogpost_list.html"), name='blogpost'),
+   path('', views.index.as_view(template_name="Blog/blogpost_list.html"), name='blogpost'),
     path('<slug:pk>/authors/', views.authors.as_view(), name='authors'),
  ]
